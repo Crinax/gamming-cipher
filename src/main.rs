c13e91b9019946fca364702ad0d62084ee566919
Kirill Sedyshev <agcrinax@gmail.com>
2023-12-01 13:48:18 +0300

    feat(crypto-bigint): add crypto bigint

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use crypto_bigint::Uint;
+
 fn main() {
-    println!("Hello, world!");
+    println!("{}", Uint::<1>::new([crypto_bigint::Limb::from(0u8)]) ^ Uint::<1>::from([crypto_bigint::Limb::from(1u8)]));
 }
